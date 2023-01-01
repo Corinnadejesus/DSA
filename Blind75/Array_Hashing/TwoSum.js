76501de150eabbbef364d448c601447496cb93cc
Corinna <Corinnadejesus@gmail.com>
2023-01-01 08:44:59 -0500

    added twosum

---
+++ Blind75/Array_Hashing/TwoSum.js
@@ -0,0 +1,48 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+//Hashmap
+var twoSum = function (nums, target) {
+  //init map
+  const hashMap = {};
+  //iterate over nums array
+
+  for (let i = 0; i < nums.length; i++) {
+    //get the value needed by subtracting target from current element of array
+    const numNeeded = target - nums[i];
+
+    //if needed value is a key in the map
+    if (hashMap[numNeeded] !== undefined) {
+      //return [needed value, current index]
+      return [numNeeded, i]
+    }
+    //otherwise set the needed value as a key in the map and store the value as an index if needed to search for later
+    hashMap[numNeeded] = i
+  }
+};
+
+//////////////////******************* ALTERNATE ********************///////////////////////
+
+//Brute Force
+var twoSum = function (nums, target) {
+  //iterate over the array of nums
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      //determine if first and second elements add up to target
+      if (nums[i] + nums[j] === target) {
+        // return indexes
+        return [i, j];
+      }
+    }
+  }
+};
---
+++ Blind75/Array_Hashing/ValidAnagram.js
@@ -0,0 +1,38 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+var isAnagram = function(s, t) {
+    //check the case if the length is not equal
+    if(s.length !== t.length) return false
+
+    //init hashmap
+    let hashMap = {}
+
+    //iterate over s length reference
+    for(let i of s) {
+        //set the key of hashmap to each element of s and set it at 1 since its been seen already
+        hashMap[i] = (hashMap[i] || 0) + 1
+    }
+
+    //iterate over t length
+    for(let j of t) {
+        //check if element of t has not been seen in hashmap
+        if(!hashMap[j] || hashMap[j] === 0) {
+            return false
+        } else {
+            //if it has been seen decrease the value from the hashmap and check the next letter
+            hashMap[j]--
+        }
+    }
+    return true
+
+};
