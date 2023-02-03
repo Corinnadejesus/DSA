c7558a5259e70dcb84518fb7a8c2f66120177dfe
Corinna <Corinnadejesus@gmail.com>
2023-02-02 22:40:37 -0500

    added prompt

--- Blind75/LinkedList/MergeTwoSortedLists.js
+++ Blind75/LinkedList/MergeTwoSortedLists.js
@@ -1,9 +1,19 @@
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
 
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+Time: O(n + m) where list1 (n) and list2 (m)
+Space: O(1) because of simple pointers shift, not creating anything that will scale as input gets larger
+*/
 
 var mergeTwoLists = function(list1, list2) {
-    //Time: O(n + m) where list1 (n) and list2 (m)
-    //Space: O(1) because of simple pointers shift, not creating anything that will scale as input gets larger
-
     let dummy = new ListNode(-Infinity)
     //we want a reference to dummy in prev because it is going to be returned at the end
     let prev = dummy
