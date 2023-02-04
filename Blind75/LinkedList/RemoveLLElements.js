73218084bfebd9438371447f7984fab12ebb669b
Corinna <Corinnadejesus@gmail.com>
2023-02-03 20:46:39 -0500

    made edits to linkedlist problems

--- Blind75/LinkedList/RemoveLLElements.js
+++ Blind75/LinkedList/RemoveLLElements.js
@@ -19,8 +19,9 @@ var removeElements = function (head, val) {
 
            [1, 2,  3,  4, 5, 6]
                P  PN   C  CN
-           the C pointer from 6 is remove because its equals the val
+           the C pointer from 6 is removed because its equals the val
            the prev.next becomes the new pointer at 6 and 3 becomes new prev.next after 6 is removed
+           C pointer is not set to the next value of 4
         */
       prev.next = curr.next;
       curr = curr.next; //increment current
--- Blind75/LinkedList/ReverseLinkedList2.js
+++ Blind75/LinkedList/ReverseLinkedList2.js
@@ -3,31 +3,54 @@ Given the head of a singly linked list and two integers left and right where lef
 
 Input: head = [1,2,3,4,5], left = 2, right = 4
 Output: [1,4,3,2,5]
+
+Time: O(n) | Space: O(1)
+
+Approach
+  - Pre-reversal
+    - point starting and current nodes at proper places
+    - set your list to hold reversals and tail as a beginning pointer in list
+
+  - Reversal
+    - While the position points towards the node you want to reverse, swap them to reverse and continue pointer
+
+  - Formulate reverseList (easy part)
+    - set your start, the reversed elements, and your tail
+
 */
 
+
 var reverseBetween = function (head, left, right) {
-  let current = head,
-    start = head,
-    position = 1;
+  let current = head;
+  let start = head;
+  position = 1;
+
 
   while (position < left) {
-    start = current;
-    current = current.next;
+    start = current; //set start at 1, the node prior to where reversal starts
+    current = current.next; //set current at 2
     position++;
   }
 
-  let reversedList = null,
-    tail = current;
+  let reversedList = null;
+  //set a tail pointer to point to beginning of reversed list
+  let tail = current;
 
+  //  Position 2 === left(2) && Position 2 < right(4)
   while (position >= left && position <= right) {
-    const next = current.next;
+    //Reverse the middle elements (2, 3, 4) to (4, 3, 2)
+    const temp = current.next;
     current.next = reversedList;
     reversedList = current;
-    current = next;
+    current = temp; //5
     position++;
   }
+
+  //2           //4,3,2
   start.next = reversedList;
+  //2           //5
   tail.next = current;
 
+  // if left is equaled to 1 there is no start value set (so return the head as the start)
   return left > 1 ? head : reversedList;
 };
