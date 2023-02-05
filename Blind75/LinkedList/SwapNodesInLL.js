2c09fc4e93bf9d2d4703d93c27ab4b758f25091b
Corinna <Corinnadejesus@gmail.com>
2023-02-04 23:02:35 -0500

    leetcode medium swapNodesinLL

---
+++ Blind75/LinkedList/SwapNodesInLL.js
@@ -0,0 +1,51 @@
+/*
+You are given the head of a linked list, and an integer k.
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,(8,7),3,0,9,5]
+
+Time: O(n) | Space: O(n)
+
+Approach
+    - Find Front at kth node
+        - loop through, increment count to get length of list, when count equals k, we found kth node
+    - Find End
+        - set end to head, iterate current until end of linked list
+        - when curr === null the end node has been found
+
+         B
+[7,9,6,6,7,8,3,0,9,5]
+           E       C
+*/
+var swapNodes = function (head, k) {
+  let curr = head;
+  let count = 0;
+  let kBegin = null;
+  let kEnd = null;
+  while (curr !== null) {
+    count++;
+
+    //this means the Kth node from the beginning has been found
+    if (kEnd !== null) {
+      //look for Kth node from the end now
+      kEnd = kEnd.next;
+    }
+
+    //curr is pointing to the Kth node from the beginning
+    if (count === k) {
+      kBegin = curr;
+      kEnd = head;
+    }
+
+    //move curr to the next node
+    curr = curr.next;
+  }
+
+  //We now have Front and End nodes, time to swap them
+  let temp = kBegin.val;
+  kBegin.val = kEnd.val;
+  kEnd.val = temp;
+
+  return head;
+};
--- Blind75/SlidingWindow/LongestSubstringNoRepeatChars.js
+++ Blind75/SlidingWindow/LongestSubstringNoRepeatChars.js
@@ -21,26 +21,50 @@ Time: O(n) | Space: O(1)
 */
 
 function lengthOfLongestSubstring(s) {
-    let begin = 0
-    let max = 0
+  let begin = 0;
+  let max = 0;
 
-    let set = new Set()
-    for (let end = 0; end < s.length; end++) {
-        //while the set contains repeat chars
-        while (set.has(s[end])) {
-            //delete the repeating chars from the beginning
-            set.delete(s[begin])
-            //update the beginning by starting at the value after the repeating char
-            begin = begin + 1
-        }
-
-        //otherwise add the letters that are not repeating
-        set.add(s[end])
-
-        //One-off error:
-        //the difference in (end, begin) gets one Index, add one to get the next non-repeat char.
-         max = Math.max(max, end - begin + 1)
+  let set = new Set();
+  for (let end = 0; end < s.length; end++) {
+    //while the set contains repeat chars
+    while (set.has(s[end])) {
+      //delete the repeating chars from the beginning
+      set.delete(s[begin]);
+      //update the beginning by starting at the value after the repeating char
+      begin = begin + 1;
     }
 
-    return max
+    //otherwise add the letters that are not repeating
+    set.add(s[end]);
+
+    //One-off error:
+    //the difference in (end, begin) gets one Index, add one to get the next non-repeat char.
+    max = Math.max(max, end - begin + 1);
+  }
+
+  return max;
+}
+
+//**************** ALTERNATE w/ Map ***************************/
+
+function lengthOfLongestSubstring(s) {
+  let begin = 0;
+  let max = 0;
+  let map = {};
+
+  //iterate over string
+  for (let end = 0; end < s.length; end++) {
+    //if ele is repeating in map and if it is located after the begining of the string
+    if (map[s[end]] !== undefined && map[s[end]] >= begin) {
+      //update the beginning point to remove that repeating char
+      begin = map[s[end]] + 1;
+    }
+    //otherwise set the value as that char as its been seen
+    map[s[end]] = end;
+
+    //update the max value as the difference between the beginning and the end + 1 to get the second value
+    max = Math.max(max, end - begin + 1);
+  }
+
+  return max;
 }
