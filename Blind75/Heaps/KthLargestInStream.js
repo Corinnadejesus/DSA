4b0d32ce9f85e13c8050536b486ea7ee28ad86a4
Corinna <Corinnadejesus@gmail.com>
2023-02-11 18:39:51 -0500

    edits made to KthLargest

--- Blind75/Heaps/KthLargestInStream.js
+++ Blind75/Heaps/KthLargestInStream.js
@@ -1,44 +1,56 @@
 /*
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+Implement KthLargest class:
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
 
+Example:
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
 
-Time: O(n * log n)
-    - log n from from popping values off based on k (n) size of the heap
 Space: O(n)
 */
 
+//Time: O(n * log n) -> log n from popping values off based on k (n) size of the heap
 var KthLargest = function (k, nums) {
   this.k = k;
-  this.minpq = new MinPriorityQueue();
+  this.minPQ = new MinPriorityQueue();
+
   for (let i = 0; i < nums.length; i++) {
-    if (this.minpq.size() < k) {
-      this.minpq.enqueue(nums[i]);
+    if (this.minPQ.size() < k) {
+      //add values to the queue as long as the size is less than k
+      this.minPQ.enqueue(nums[i]);
     } else if (
-      this.minpq.size() === k &&
-      nums[i] >= this.minpq.front().element
+      this.minPQ.size() === k &&
+      nums[i] >= this.minPQ.front().element
     ) {
-      this.minpq.enqueue(nums[i]);
-      this.minpq.dequeue();
+      //remove the smallest elements that appear at the front of heap until it equals the size of k
+      this.minPQ.enqueue(nums[i]);
+      this.minPQ.dequeue();
     }
   }
 };
 
+//Time: O(n * log k) -> log n from adding and removing values of the heap based on (n) number of function calls to add()
 KthLargest.prototype.add = function (val) {
-  if (this.minpq.size() < this.k) {
-    this.minpq.enqueue(val);
-  } else if (val > this.minpq.front().element) {
-    this.minpq.enqueue(val);
-    this.minpq.dequeue();
+  if (this.minPQ.size() < this.k) {
+    this.minPQ.enqueue(val);
+  } else if (val > this.minPQ.front().element) {
+    this.minPQ.enqueue(val);
+    this.minPQ.dequeue();
   }
-  return this.minpq.front().element;
+  return this.minPQ.front().element;
 };
 
 /* ALTERNATE */
 
+//Time: O(n * log n) | Space: O(n)
 var KthLargest = function (k, nums) {
   this.k = k;
   this.arr = nums.sort((a, b) => a - b);
 };
 
+//Time: O(log n) -> Binary Search | Space: O(1)
 KthLargest.prototype.add = function (val) {
   const insert = () => {
     let l = 0,
