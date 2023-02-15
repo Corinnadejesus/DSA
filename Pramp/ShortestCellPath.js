dd7aa4e8895ae7205828711793f557f22e3bcae6
Corinna <Corinnadejesus@gmail.com>
2023-02-15 09:35:44 -0500

    graph file restructuring, pramp questions added

---
+++ Pramp/ShortestCellPath.js
@@ -0,0 +1,51 @@
+/*
+In a given grid of 0s and 1s, we have some starting row and column sr, sc and a target row and column tr, tc. Return the length of the shortest path from sr, sc to tr, tc that walks along 1 values only.
+
+Each location in the path, including the start and the end, must be a 1. Each subsequent location in the path must be 4-directionally adjacent to the previous location.
+It is guaranteed that grid[sr][sc] = grid[tr][tc] = 1, and the starting and target positions are different.
+If the task is impossible, return -1.
+
+Examples:
+input:
+grid = [[1, 1, 1, 1], [0, 0, 0, 1], [1, 1, 1, 1]]
+sr = 0, sc = 0, tr = 2, tc = 0
+output: 8
+(The lines below represent this grid:)
+1111
+0001
+1111
+
+grid = [[1, 1, 1, 1], [0, 0, 0, 1], [1, 0, 1, 1]]
+sr = 0, sc = 0, tr = 2, tc = 0
+output: -1
+(The lines below represent this grid:)
+1111
+0001
+1011
+
+Time: O(r * c) | Space: O(r * c) -> the space to store the queue and seen
+
+Approach
+    - Finding a shortest path is typically done with a breadth first search. Here, nodes are locations on the grid with value 1, and two nodes are neighbors if they are 4-directionally adjacent.
+
+    - The breadth first search algorithm is given a source in the graph, and it explores all nodes distance 0 from the source, then all nodes distance 1, then all nodes distance 2, and so on. The algorithm records the node’s distance when it visits, and that way we can determine the shortest path in the graph to some target node.
+
+    - By visiting nodes in order from distance to the source, this ensures that if we find the target word, we found it at the least possible distance and thus the answer is correct.
+*/
+
+// function shortestCellPath(grid, sr, sc, tr, tc) {
+//     let queue = [sr, sc, 0]
+//     queue.add((sr, sc, 0))
+//     let seen = new Map()
+//     seen.add((sr, sc))
+
+//     while (queue !== null) {
+//         r, c, depth = queue.popfront()
+//         if r == tr and c == tc: return depth
+//         for (nr, nc) in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
+//             if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] == 1 and (nr, nc) not in seen:
+//                 queue.append((nr, nc, depth + 1))
+//                 seen.add((nr, nc))
+//     }
+//     return -1
+// }
---
+++ Pramp/SmallestSubstringAllChars.js
@@ -0,0 +1,72 @@
+/*
+
+
+
+
+Time: O(n + m) | Space: O(n)
+  Approach
+    - Scan input str at head and tail index
+    - Iterate over input string
+    - Create a temp substring with (head, head + 1, tail)
+    - Increment tail until the temp has every unique char
+    - If the temp === arr.length we have the shortest valid substring (return temp)
+    - Otherwise, Increment head index as long as the temp substring remains valid
+    - If the temp is shorter than the previous substring, update it to be the current substring
+*/
+
+function getShortestUniqueSubstring(arr, str) {
+  let headIndex = 0;
+  let result = "";
+  let uniqueCounter = 0;
+  let countMap = {};
+
+  // initialize countMap
+  for (let i = 0; i < arr.length; i++) {
+    countMap[arr[i]] = 0;
+  }
+
+  // scan str
+  for (let tailIndex = 0; tailIndex < str.length; tailIndex++) {
+    // handle the new tail
+    let tailChar = str[tailIndex];
+
+    // skip all the characters not in arr
+    if (!(tailChar in countMap)) {
+      continue;
+    }
+
+    let tailCount = countMap[tailChar];
+    if (tailCount == 0) {
+      uniqueCounter++;
+    }
+
+    countMap[tailChar]++;
+
+    // push head forward
+    while (uniqueCounter == arr.length) {
+      let tempLength = tailIndex - headIndex + 1;
+      if (tempLength == arr.length) {
+        // return a substring of str from headIndex to tailIndex(inclusive)
+        return str.slice(headIndex, tailIndex + 1);
+      }
+
+      if (result === "" || tempLength < result.length) {
+        // update result
+        result = str.slice(headIndex, tailIndex + 1);
+      }
+
+      let headChar = str[headIndex];
+
+      if (headChar in countMap) {
+        let headCount = countMap[headChar] - 1;
+        if (headCount === 0) {
+          uniqueCounter--;
+        }
+        countMap[headChar]--;
+      }
+
+      headIndex++;
+    }
+  }
+  return result;
+}
