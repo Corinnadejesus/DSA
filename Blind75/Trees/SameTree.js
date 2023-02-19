f019c49fa6af2264b427158a9c7f325c5ad26d61
Corinna <Corinnadejesus@gmail.com>
2023-02-18 19:21:21 -0500

    another leetcode easy trees

---
+++ Blind75/Trees/SameTree.js
@@ -0,0 +1,31 @@
+/*
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+*/
+
+//DFS
+//Time: O(n) | Space: O(1)
+var isSameTree = function (p, q) {
+  if (!p && !q) return true;
+  if ((p && !q) || (q && !p) || p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+//BFS
+//Time: O(n) | Space: O(n)
+var isSameTree = function (p, q) {
+  const stack = [p, q];
+  while (stack.length) {
+    n2 = stack.pop();
+    if (!n1 && !n2) continue;
+    if (!n1 || !n2 || n1.val !== n2.val) return false;
+    stack.push(n1.left, n2.left, n1.right, n2.right);
+  }
+  return true;
+};
