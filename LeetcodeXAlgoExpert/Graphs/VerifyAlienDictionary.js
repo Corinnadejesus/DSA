032ba8aa92daa36c70139985185c006fac12cf58
Corinna <Corinnadejesus@gmail.com>
2023-03-01 12:07:20 -0500

    updated problem with two solutions

--- LeetcodeXAlgoExpert/Graphs/VerifyAlienDictionary.js
+++ LeetcodeXAlgoExpert/Graphs/VerifyAlienDictionary.js
@@ -14,7 +14,28 @@ Output: false
 Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character
 
 Time: O(n^2)| Space: O(n)
+
+Approach
+  - If the first word is a prefix of the second word then second work must be after first word (return true).
+  - If second word ends before first word and there are no different letters (return false).
+  - If we find the first different letter and the two words are in the wrong order (return false).
 */
+
+var isAlienSorted = function (words, order) {
+  for (let i = 0; i < words.length - 1; i++) {
+    // Loop all words
+    for (let j = 0; j < words[i].length; j++) {
+      // Loop all chars of word[i]
+      const indexCurrent = order.indexOf(words[i][j]); // Get index of char at j of current word
+      const indexNext = order.indexOf(words[i + 1][j]); // Get index of char at j of next word
+      if (indexCurrent < indexNext) break; // Break for-loop if indexCurrent is smaller than indexNext
+      if (indexCurrent > indexNext) return false; // Return false if indexCurrent is greater than indexNext (not sorted in alien language)
+    }
+  }
+  return true; // Return true (given words are sorted lexicographically in given alien language)
+};
+
+///////*********** ALTERNATE *********//////////////
 
 var isAlienSorted = function (words, order) {
   //create map of dictionary
@@ -26,13 +47,15 @@ var isAlienSorted = function (words, order) {
   //loop to run through words array
   for (let i = 1; i < words.length; i++) {
     //loop to run through each index words array
+    //[i - 1] -> we want to stop at the second to last word to compare every adjacent pair of words
     for (let j = 0; j < words[i - 1].length; j++) {
-      //check if first word has letter but decond word is empty
+      //if first word has letter but second word is empty
       if (words[i - 1][j] && words[i][j] === undefined) return false;
 
-      // check if both words occurence is equal or not
+      //if the word pairs are equal
       if (dicMap[words[i - 1][j]] === dicMap[words[i][j]]) {
         continue;
+        //if the first word is larger than the second word
       } else if (dicMap[words[i - 1][j]] > dicMap[words[i][j]]) {
         return false;
       } else {
