0e4b2d8201e91140c312a3e8a6c8fe782399f4ea
Corinna <Corinnadejesus@gmail.com>
2023-03-11 15:01:05 -0500

    edited problem walk through and solution

--- LeetcodeXAlgoExpert/Pointers/ThreeSum.js
+++ LeetcodeXAlgoExpert/Pointers/ThreeSum.js
@@ -22,55 +22,54 @@ Output: [[0,0,0]]
 Explanation: The only possible triplet sums up to 0.
 
 Time: O(n^2) | Space: O(n)
+
+Approach
+GOAL => nums[i] + nums[j] + nums[k] == 0
+TARGET VALUE => nums[j] + nums[k] = 0 - nums[i]
+    - Sort the input and Iterate
+    - If the curr val is a previous duplicate skip it
+    - Set target value, left and right pointers
+    - While left is less than right
+        - If the sum of left, right, and curr is less than 0 -> increment left
+        - Else if its greater -> decrement right
+        - Else we found a triplet
+            - Add array of trips (left, right, curr) to holdTrips array
+            - while duplicates exist on left side -> increment left
+            - while duplicates exist on right side -> decrement right
+            - Decrement right, increment left to look for other triplets
 */
 
 //Approach: Two Sum with Added Steps
-// nums[i] + nums[j] + nums[k] == 0 => GOAL
-// nums[j] + nums[k] = 0 - nums[i] => TARGET VALUE
 
 var threeSum = function (nums) {
-  //sort the array
   nums.sort((a, b) => a - b);
 
-  //init array to hold triplets
   const holdTrips = [];
 
-  //loop over array
   for (let i = 0; i < nums.length; i++) {
-    //check if there are duplicates continue
     if (i > 0 && nums[i] === nums[i - 1]) continue;
 
-    //**** Two Pointer Solution Starts Here ****
     let target = 0 - nums[i];
     let left = i + 1;
     let right = nums.length - 1;
 
-    //while left is less than right
     while (left < right) {
       const sum = nums[left] + nums[right];
-      //if sum is greater than target move right side (backward) towards the center
       if (sum > target) {
         right--;
-        //if sum is less than target move left side (forward) towards the center
       } else if (sum < target) {
         left++;
       } else {
-        //otherwise
-        //push (array) of the triplets into holdTrips
         holdTrips.push([nums[i], nums[left], nums[right]]);
 
-        //check for duplicates within the array on both left and right sides
-        //while the LEFT current value is equal to previous value => increment left to skip duplicate
         while (nums[left] === nums[left + 1]) {
           left++;
         }
 
-        //while the RIGHT current value is equal to previous value => decrement right to skip duplicate
         while (nums[right] === nums[right - 1]) {
           right--;
         }
 
-        //increment left and right again to continue to look for triplets
         left++;
         right--;
       }
