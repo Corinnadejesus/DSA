24a0e0d046d52a551f5f87d7767c9255665a5183
Corinna <Corinnadejesus@gmail.com>
2023-03-11 11:29:48 -0500

    edited problem walk through and solution

--- LeetcodeXAlgoExpert/SlidingWindow/LongestSubstringNoRepeatChars.js
+++ LeetcodeXAlgoExpert/SlidingWindow/LongestSubstringNoRepeatChars.js
@@ -18,52 +18,36 @@ Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 Time: O(n) | Space: O(1)
+
+Approach
+  - Set left and right pointers
+  - Starting from right side over string
+    - Populate our map with non-repeating chars
+    - Check for repeating chars, if so delete them and move left forward
+  - Updating the maximum substring length at every iteration
 */
 
 function lengthOfLongestSubstring(s) {
-  let begin = 0;
+  let left = 0;
   let max = 0;
 
   let set = new Set();
-  for (let end = 0; end < s.length; end++) {
+  for (let right = 0; right < s.length; right++) {
     //while the set contains repeat chars
-    while (set.has(s[end])) {
-      //delete the repeating chars from the beginning
-      set.delete(s[begin]);
-      //update the beginning by starting at the value after the repeating char
-      begin = begin + 1;
+    while (set.has(s[right])) {
+      //delete the repeating chars from the left and move left forward
+      set.delete(s[left]);
+      left++
     }
 
-    //otherwise add the letters that are not repeating
-    set.add(s[end]);
+    //otherwise add the letters that are not repeating to set
+    set.add(s[right]);
 
     //One-off error:
-    //the difference in (end, begin) gets one Index, add one to get the next non-repeat char.
-    max = Math.max(max, end - begin + 1);
-  }
-
-  return max;
-}
-
-//**************** ALTERNATE w/ Map ***************************/
-
-function lengthOfLongestSubstring(s) {
-  let begin = 0;
-  let max = 0;
-  let map = {};
-
-  //iterate over string
-  for (let end = 0; end < s.length; end++) {
-    //if ele is repeating in map and if it is located after the beginning of the string
-    if (map[s[end]] !== undefined && map[s[end]] >= begin) {
-      //update the beginning point to remove that repeating char from the map
-      begin = map[s[end]] + 1;
-    }
-    //otherwise set the value as that char as its been seen
-    map[s[end]] = end;
-
-    //update the max value as the difference between the beginning and the end + 1 to get the second value
-    max = Math.max(max, end - begin + 1);
+    // 0, 1, 2
+    // a  b  c
+    //right - left + 1 = 3
+    max = Math.max(max, right - left + 1);
   }
 
   return max;
