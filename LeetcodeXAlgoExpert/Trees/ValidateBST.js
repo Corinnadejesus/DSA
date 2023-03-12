d9a73136f31422678be4738312e220be010dbda3
Corinna <Corinnadejesus@gmail.com>
2023-03-11 22:20:39 -0500

    updated solution

--- LeetcodeXAlgoExpert/Trees/ValidateBST.js
+++ LeetcodeXAlgoExpert/Trees/ValidateBST.js
@@ -17,24 +17,24 @@ Time: O(n) | Space: O(h)
 
 Recursive DFS Approach
     - We need to compare the left and right subtrees, but also compare the root to something
-    - Initially compare the root to (-Infinity for left, Infinity for right)
+    - Initally compare the root to (-Infinity for left, Infinity for right)
     - Create our dfs helper function
         - BC: if root is null return true (empty trees are valid BST)
         - BC: if root >= min || root <= max return false
 
         return boolean calling dfs on both left, min and right, max
-    - Call dfs on root
-*/
+    - Call dfs on isValidBST passing in arguments
 
+*/
 var isValidBST = function (root) {
   function dfs(root, min, max) {
     if (!root) return true;
-    if (root >= max || root <= min) return false;
+    if (root.val >= max || root.val <= min) return false;
 
-    return dfs(root.left, min, root.val) && dfs(root.right, max, root.val);
+    return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
   }
 
-  return dfs(root);
+  return dfs(root, -Infinity, Infinity);
 };
 
 //Iterative DFS
