1d85c1229e9431a27fcbb03a1406c3709b0afc3e
Corinna <Corinnadejesus@gmail.com>
2023-03-14 14:11:01 -0400

    added walkthrough

--- LeetcodeXAlgoExpert/Trees/ValidateBST.js
+++ LeetcodeXAlgoExpert/Trees/ValidateBST.js
@@ -6,32 +6,39 @@ A valid BST is defined as follows:
     - The right subtree of a node contains only nodes with keys greater than the node's key.
     - Both the left and right subtrees must also be binary search trees.
 
-Input: root = [2,1,3]
-Output: true
-
-Input: root = [5,1,4,null,null,3,6]
+Input: root = [10,5,15,3,7,null,13]
 Output: false
-Explanation: The root node's value is 5 but its right child's value is 4.
+Explanation: The right root node's value is 15 but its right child's value is 13.
 
 Time: O(n) | Space: O(h)
 
 Recursive DFS Approach
-    - We need to compare the left and right subtrees, but also compare the root to something
-    - Initially compare the root to (-Infinity for left, Infinity for right)
-    - Create our dfs helper function
-        - BC: if root is null return true (empty trees are valid BST)
-        - BC: if root >= min || root <= max return false
+            10
+          /   \
+         5    15
+        / \    \
+       3  7    13
 
-        return boolean calling dfs on both left, min and right, max
-    - Call dfs on isValidBST passing in arguments
+    - If current node's value is not between min and max values its not valid
+    - Left ( 3 > -I )
+        -I < 3 > 5
+        min     max
+
+    - Right ( 7 > 5)
+        5 < 7 < I
+       min     max
 
 */
 var isValidBST = function (root) {
   function dfs(root, min, max) {
     if (!root) return true;
     if (root.val >= max || root.val <= min) return false;
-
-    return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
+    return (
+      //parent (root.val) is updated as the left boundary
+      dfs(root.left, min, root.val) &&
+      //parent (root.val) is updated as the right boundary
+      dfs(root.right, root.val, max)
+    );
   }
 
   return dfs(root, -Infinity, Infinity);
