a444a9ed6e097e9f5451b5cc4a33f527404fd713
Corinna <Corinnadejesus@gmail.com>
2023-04-03 23:34:44 -0400

    leetcode medium array

--- LeetcodeXAlgoExpert/Array_Hashing/ProductExceptItself.js
+++ LeetcodeXAlgoExpert/Array_Hashing/ProductExceptItself.js
@@ -7,11 +7,38 @@ Example 1:
 Input: nums = [1,2,3,4]
 Output: [24,12,8,6]
 
-Example 2:
-Input: nums = [-1,1,0,-3,3]
-Output: [0,0,9,0,0]
+Approach
+Left:   [1,1,2,6]
+Right: [24,12,4,1]
+Multiply pre * post
 */
 
+//Time: O(n) | Space: O(n)
+function productExceptSelf(nums) {
+  const result = new Array(nums.length).fill(1);
+
+  let prefix = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  let postfix = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= postfix;
+    postfix *= nums[i];
+  }
+
+  return result;
+}
+
+
+
+////////////////////*************** ALTERNATIVE **************////////////////
+
+
+
+//Time: O(n) | Space: O(n)
 function productExceptSelf(nums) {
   const prefix = new Array(nums.length).fill(1);
   const postfix = new Array(nums.length).fill(1);
@@ -46,22 +73,3 @@ function productExceptSelf(nums) {
 
   return result;
 }
-
-// Optimal space
-// function productExceptSelf(nums) {
-//   const result = new Array(nums.length).fill(1);
-
-//   let prefix = 1;
-//   for (let i = 0; i < nums.length; i++) {
-//     result[i] = prefix;
-//     prefix *= nums[i];
-//   }
-
-//   let postfix = 1;
-//   for (let i = nums.length - 1; i >= 0; i--) {
-//     result[i] *= postfix;
-//     postfix *= nums[i];
-//   }
-
-//   return result;
-// }
