004fd442e5d00c9617b0278731628722aeda9382
Corinna <Corinnadejesus@gmail.com>
2023-05-14 21:04:07 -0400

    updated solution

--- LeetcodeXAlgoExpert/LinkedList/RemoveDuplicatesInLL.js
+++ LeetcodeXAlgoExpert/LinkedList/RemoveDuplicatesInLL.js
@@ -1,5 +1,5 @@
 /*
-Write a function that returns a modified version of a Linked List that does'n't contain any duplicate nodes.
+Write a function that returns a modified version of a Linked List that doesn't contain any duplicate nodes.
 Linked List should be modified in-place and should have nodes sorted in respect to their values.
 
 Example
@@ -9,29 +9,19 @@ Output: [1, 3, 4, 5, 6]
 Time: O(n) | Space: O(1)
 */
 
-//Given
-class LinkedList {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
 function removeDuplicatesFromLinkedList(linkedList) {
   let curr = linkedList;
   while (curr !== null) {
     let uniqueNode = curr.next;
-    // if first and second value are the same Ex. [1, 1, 3]
     while (uniqueNode !== null && uniqueNode.value === curr.value) {
-      //everytime we see a duplicate move the next value
+      //Ex. [1, 1, 3]
+      //           U
       uniqueNode = uniqueNode.next;
     }
     // once we found all duplicate nodes, now we remove them by removing the pointer to them
     // [1, 1,  3]
     //  C  CN  U
 
-    // [1, 3, 4]
-    //  P  C
     curr.next = uniqueNode;
     curr = uniqueNode;
   }
