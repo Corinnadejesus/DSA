7454d7ec5c67933c2fcfc3a7420efec55b73dc35
Corinna <Corinnadejesus@gmail.com>
2023-05-15 16:15:39 -0400

    added examples and summarized approach

--- LeetcodeXAlgoExpert/LinkedList/LinkedListCycle.js
+++ LeetcodeXAlgoExpert/LinkedList/LinkedListCycle.js
@@ -3,16 +3,17 @@ Given head, the head of a linked list, determine if the linked list has a cycle 
 There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 Return true if there is a cycle in the linked list. Otherwise, return false.
 
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail (-4) connects to the 1st node (2) (0-indexed).
+
 Time: O(n) | Space: O(1)
+
 Approach
-Init slow pointer at head
-Init fast pointer at head
-Iterate while fast is not null
-    if fast pointer's next value is null value return false
-    move slow pointer forward by one
-    move fast pointer forward by two
-    check if slow and fast are equal, if they are we have a cycle return true
-Otherwise we dont and return false
+  - Start slow and fast pointers at head
+  - Loop until fast is null
+    - Moving slow by one and fast by two
+    - If fast and slow meet at any point, there is a cycle
 */
 
 var hasCycle = function (head) {
