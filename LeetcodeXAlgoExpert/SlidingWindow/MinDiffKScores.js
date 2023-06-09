63c9306c9cecfee9133462dabc871a35ba2916d9
Corinna <Corinnadejesus@gmail.com>
2023-06-08 20:30:15 -0400

    corrected space complex typo and added pseudocode for clarification

--- LeetcodeXAlgoExpert/SlidingWindow/MinDiffKScores.js
+++ LeetcodeXAlgoExpert/SlidingWindow/MinDiffKScores.js
@@ -1,14 +1,17 @@
 /*
 You are given a 0-indexed integer array nums, where nums[i] represents the score of the ith student. You are also given an integer k.
 
-Pick the scores of any k students from the array so that the difference between the highest and the lowest of the k scores is minimized.
+Pick the scores of any k students from the array and return the minimum possible difference between the highest and lowest of the k scores.
 
-Return the minimum possible difference.
 Input: nums = [9,4,1,7], k = 2
 Output: 2
 Explanation: There are six ways to pick score(s) of two student, the minimum possible difference is 2.
 
-Time: O(n log n) | Space: O1
+Time: O(n log n) | Space: O(1)
+
+Approach
+  - Loop through sorted input
+    - At every iteration subtract the larger right digit from the smaller left digit and update minVal to the lowest value
 */
 
 var minimumDifference = function (nums, k) {
