21ca596f897c7b284d77fbf5465d85f86a887af5
Corinna <Corinnadejesus@gmail.com>
2023-08-11 09:35:28 -0400

    added O(n) memory solution

--- LeetcodeXAlgoExpert/Pointers/RotateArray.js
+++ LeetcodeXAlgoExpert/Pointers/RotateArray.js
@@ -8,11 +8,9 @@ Explanation:
 rotate 1 steps to the right: [7,1,2,3,4,5,6]
 rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
-
-Time: O(n)
-Space: O(1)
 */
 
+//Time: O(n) | Space: O(1)
 var rotate = function (nums, k) {
   k = k % nums.length;
 
@@ -32,4 +30,20 @@ var rotate = function (nums, k) {
 
   //[5,6,7,1,2,3,4]
   helper(k, nums.length - 1);
+};
+
+//////*******************************************************************///////
+
+//Time: O(n) | Space: O(n)
+var rotate = function (nums, k) {
+  let arr = [];
+  k = k % nums.length;
+
+  for (i = 0; i < nums.length; i++) {
+    //for each element, a new index is calculated after rotation and stored in arr
+    //this prevents going out of bounds
+    arr[(i + k) % n] = nums[i];
+  }
+
+  return arr;
 };
